d775722453a2adfe1d4cd782ab66da570c0ff867
Brandon Miller <brandon.miller@hashicorp.com>
2022-04-13 19:20:22 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/bm_mi_lab/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  network_name = "bm_net"
+  project_id = "var.project"
+  subnets = [
+  {
+    subnet_name   = "bm_net-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
